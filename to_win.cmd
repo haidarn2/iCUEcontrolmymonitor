2264e052da9ca8cbe96306b648f67180f2e5c173
Nadeem Haidar <nadeem_haidar@hotmail.com>
2021-10-14 14:59:34 -0400

    initial commit

---
+++ to_win.cmd
@@ -0,0 +1,2 @@
+start C:\"Program Files (x86)"\Corsair\"CORSAIR iCUE Software"\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
+start C:\"Program Files (x86)"\Corsair\"CORSAIR iCUE Software"\controlmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 15
