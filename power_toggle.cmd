fc51390a497f643bcdc26aefc2f0d4a0aa3ea7e2
Nadeem Haidar <nadeem_haidar@hotmail.com>
2022-01-07 01:11:53 -0500

    update scripts

---
+++ power_toggle.cmd
@@ -0,0 +1,2 @@
+start C:\Users\Nadeem\Documents\macros\iCUEcontrolmymonitor-main\ControlMyMonitor.exe /SwitchOffOn "\\.\DISPLAY1\Monitor0"
+start C:\Users\Nadeem\Documents\macros\iCUEcontrolmymonitor-main\ControlMyMonitor.exe /SwitchOffOn "\\.\DISPLAY2\Monitor0"
