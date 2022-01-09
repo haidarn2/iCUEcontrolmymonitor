7f08ff6c8902fed1ac02f1bf609caf5cf2f81fd0
Nadeem Haidar <nadeem_haidar@hotmail.com>
2022-01-09 02:02:34 -0500

    update script paths

--- to_win.cmd
+++ to_win.cmd
@@ -1,2 +1,2 @@
-start C:\Users\Nadeem\Documents\macros\iCUEcontrolmymonitor-main\ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
-start C:\Users\Nadeem\Documents\macros\iCUEcontrolmymonitor-main\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 15
+start C:\Users\Nadeem\Documents\macros\iCUEcontrolmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY1\Monitor0" 60 15
+start C:\Users\Nadeem\Documents\macros\iCUEcontrolmymonitor\ControlMyMonitor.exe /SetValue "\\.\DISPLAY2\Monitor0" 60 15
